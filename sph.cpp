26fed5aabc1ef3193e6e65fbdc8bea6521f6a3d2
John Howard <orthopteroid@gmail.com>
2017-06-30 00:07:04 -0700

    some serious fixes to the core extra features that allow hashing into large non-power-of-two tables of wider type

--- sph.cpp
+++ sph.cpp
@@ -4,6 +4,8 @@
 #include <string.h>
 #include <time.h>
 #include <math.h>
+#include <utility>
+#include <algorithm>
 
 // stochasticPerfectHash
 // Uses stochastic remainder sampling to derive a Pearson mixing table for small sets of alphabetic keys
@@ -14,77 +16,105 @@
 // http://cs.mwsu.edu/~griffin/courses/2133/downloads/Spring11/p677-pearson.pdf
 
 // stochastic remainder sampler
-size_t sample( uint8_t* counts, uint32_t sum )
+template<class TableType>
+int sample( TableType* counts, int sum )
 {
-	int32_t target = (((int32_t)rand() << 20) + ((int32_t)rand() << 10) + (int32_t)rand()) % sum; // impl fix for short int rand()
-	size_t i = 0;
-	while( (target -= counts[ i ]) > 0 ) { i++; }
-	return i;
+    int32_t target = (int32_t)rand() % sum;
+    size_t i = 0;
+    while( (target -= counts[ i ]) > 0 ) { i++; }
+    return i;
+}
+
+template<class TableType, int TableSize>
+int sum( TableType* counts )
+{
+    uint32_t sum = 0;
+    for( size_t i=0;i<TableSize;i++ )
+    {
+        sum += counts[i];
+    }
+    return sum;
 }
 
 // calculates a mixtable for the pearson hash to create a perfect hash.
-// restrictions are keys can only be ascii lowercase, but can be of any length.
+// keys can only be mixed-case, but can be of any length.
 // keys are seperated in the input string using the ! delimiter. ! must also be at end of string.
+template<class TableType, int TableSize>
 size_t calcMixTable( char* sz )
 {
-	const size_t MAXITER = 25000;
+	const size_t MAXITER = 1000000;
 
-	// special marker '!' in the string designates the end of a key
-	size_t keyCount = 0;
-	for( size_t i=0;sz[i];i++ ) { if( sz[i] == '!' ) { keyCount++; } }
-	bool hashed[ keyCount ];
+    // special marker '!' in the string designates the end of a key
+    int keyCount = 0;
+    for( size_t i=0;sz[i];i++ ) { if( sz[i] == '!' ) { keyCount++; } }
 
-	// the defualt mix table is a null transform
-	uint8_t mix[32] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31};
+    TableType usedKeyHash[TableSize]; // to mark key-hashes used
+    TableType usedCode[TableSize], notUsedCode[TableSize]; // to mark ascii codes used
+    TableType mixTable[TableSize];
 
-	// these weights influence convergence rate.
-	// they are used by the stochastic sampling method that adjusts the mixing table to resolve hash collisions
-	const uint8_t DEFAULT = 1;
-	const uint8_t SIGNAL = 0xFF;
-	
+    int avgKeyLength = ( ::strlen(sz) - keyCount ) / keyCount;
+
+	// start with a random mixing
+    // thanks to https://github.com/tdewolff/hasher
+    for(int i=0;i<TableSize;i++) mixTable[i] = i;
+    for(int i=0;i<TableSize;i++) std::swap(mixTable[rand() % TableSize],mixTable[rand() % TableSize]);
+
 	// reset flags...
 	size_t iterations = 0;
 	do {
-		// counts will track mix table usage each iteration we evaluate the hashed
-		uint8_t counts[32];
-		
-		for( size_t i=0;i<32;i++ ) { counts[i] = DEFAULT; }
-		for( size_t i=0;i<keyCount;i++ ) { hashed[i] = false; }
+        for(int i=0;i<TableSize;i++) usedCode[i] = usedKeyHash[i] = 0;
 
 		// compute the pearson hash using the current mix table.
-		bool collision = false;
-		uint8_t hash = mix[ 31 & ( sz[0] - 'a' ) ]; // init using 1st char and mix table
+        TableType hash = mixTable[ sz[0] - 'a' ]; // start hash for 1st keyword
 		for( size_t i=0;sz[i];i++ )
 		{
 			if( sz[i] == '!' ) // end of key detected
 			{
-				size_t j = hash % keyCount; // mod with keyCount as we are trying to find the perfect hash function;
-				if( hashed[ j ] ) { collision = true; }
-				hashed[ j ] = true;
-				hash = mix[ 31 & ( sz[i+1] - 'a' ) ]; // init using 1st char and mix table
+                usedKeyHash[ hash % TableSize ]++; // record hash for completed key as used
+                hash = mixTable[ sz[++i] - 'a' ]; // restart hash for next keyword or null
 			}
 			else
 			{
-				uint8_t j = 31 & ( hash ^ ( sz[i] - 'a' ) );
-				hash = mix[ j ];
-				counts[ j ] = SIGNAL; // inc the mix item that was used to compute the hashs this iteration.
+				int j = (hash ^ (sz[i] - 'a')) % TableSize;
+				hash = mixTable[ j ];
+				usedCode[ j ] = 1;
 			}
 		}
+
+        // if any key-hashes have been used more than once, there is a collision
+        bool collision = false;
+        for(int i=0;i<TableSize;i++) { collision |= (usedKeyHash[i] > 1); }
 		if( !collision ) { break; }
-		
-		// calc sum and swap items
-		uint32_t countsum = 0;
-		for( size_t i=0;i<32;i++ ) { countsum += counts[i]; }
-		
-		size_t i = sample( counts, countsum );				// select item i.
-		countsum -= counts[i]; counts[i] = 0;				// remove from possible selection set
-		size_t j = sample( counts, countsum );				// select item j
-		uint8_t t = mix[i]; mix[i] = mix[j]; mix[j] = t;	// swap i and j
+
+        // mark unused codes
+        for( size_t i=0;i<TableSize;i++ ) { notUsedCode[i] = usedCode[i] ? 0 : 1; }
+
+		// calc sum and swap some of the most used with some of the least used
+		int sumUsed = sum<TableType,TableSize>(usedCode);
+        int sumNotUsed = sum<TableType,TableSize>(notUsedCode);
+
+        // if all chars are used, there isnt a way we can rearrange the mixing table
+        if( sumNotUsed == 0 ) { iterations = MAXITER; break; }
+
+        for( int k=0;k<(1+avgKeyLength);k++)
+        {
+            // select which to swap
+            int i = sample<TableType>(usedCode, sumUsed);
+            int j = sample<TableType>(notUsedCode, sumNotUsed);
+
+            // disqualify items from ever being selected again and swap
+            sumUsed -= usedCode[i] + usedCode[j]; // reduce sum
+            sumNotUsed -= notUsedCode[i] + notUsedCode[j]; // reduce sum
+            usedCode[i] = notUsedCode[i] = usedCode[j] = notUsedCode[j] = 0; // clear summable values
+            std::swap(mixTable[i], mixTable[j]);
+
+            if(sumUsed < 1 || sumNotUsed < 1) break;
+        }
 
 	} while( ++iterations < MAXITER );
 
 	if( iterations < MAXITER )
-		{ printf("uint_8 mix[] = {"); for( size_t i=0;i<32;i++ ) { printf("%d, ",mix[i]); } printf("};\n"); }
+		{ printf("uint16_t mix[] = {"); for( size_t i=0;i<TableSize;i++ ) { printf("%d, ",mixTable[i]); } printf("};\n"); }
 	else
 		{ printf("ack! can't do it.\n"); }
 	
@@ -97,15 +127,19 @@ int main()
 	time(&ltime);
 	srand((unsigned int)ltime);
 
-	char buf[1000]; // a fixed buffer for a sequence of keys we wish to perfect-hash
+    const int keyCount = 10;
+    const int keyLength = 10;
+    const int tableSize = 256;
+    using tableType = uint8_t;
+    printf("Generating %d keys of length %d. Hashing into %d element table\n", keyCount, keyLength, tableSize);
+
+	char buf[100000];
 	for( size_t k=0;k<10;k++)
 	{
-		const size_t keyCount = 10;
 		size_t pos = 0;
 		for( size_t i=0;i<keyCount;i++)
 		{
 			// keys are all ascii lowsercase
-			const size_t keyLength = 10;
 			for( size_t j=0;j<keyLength;j++) { buf[ pos++ ] = 'a' + rand() % 26; }
 
 			// keys end with special '!' char
@@ -113,11 +147,10 @@ int main()
 		}
 		buf[ pos ] = 0; // zero term buffer
 
-		printf("Finding mix table for %s:\n", buf);
-		size_t iter = calcMixTable( buf );
+        printf("Finding mix table for %s:\n", buf);
+		size_t iter = calcMixTable<tableType,tableSize>( buf );
 		printf("Took %lu iterations\n",iter);
 	}
-
 
 	return 0;
 }
